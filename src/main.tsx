1df46560eb89a6a152090897ce708c27078dd218
Matilda Mared <matildamared@live.se>
2023-10-15 08:26:07 +0200

    add msw mocking

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { worker } from "./mocks/browser.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (process.env.NODE_ENV === "mock-dev") {
+	("We are in dev mode!");
+	worker.start();
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
---
+++ src/mocks/responses.ts
@@ -0,0 +1,22 @@
+import { Cat } from "../types/Cat";
+
+export const catsResponse: Cat[] = [
+	{
+		id: 1,
+		name: "Vincent",
+		age: 4,
+		breed: "Persian",
+	},
+	{
+		id: 2,
+		name: "Tom",
+		age: 3,
+		breed: "Siamese",
+	},
+	{
+		id: 3,
+		name: "Mr. Bigglesworth",
+		age: 2,
+		breed: "Sphynx",
+	},
+];
